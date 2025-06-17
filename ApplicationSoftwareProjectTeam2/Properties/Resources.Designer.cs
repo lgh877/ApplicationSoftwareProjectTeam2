acace0d08449770ef2ddc7edf926207f8ef72c9d
Jeon Jiwon <jeonjiwon1@naver.com>
2025-06-18 03:45:29 +0900

    Human1 스프라이트

--- ApplicationSoftwareProjectTeam2/Properties/Resources.Designer.cs
+++ ApplicationSoftwareProjectTeam2/Properties/Resources.Designer.cs
@@ -491,6 +491,96 @@ namespace ApplicationSoftwareProjectTeam2.Properties {
         }
         
         /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_attack1 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_attack1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_attack2 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_attack2", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_attack3 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_attack3", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_idle1 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_idle1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_idle2 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_idle2", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_walk1 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_walk1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_walk2 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_walk2", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_walk3 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_walk3", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Byte[] 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static byte[] sprite_Human1_walk4 {
+            get {
+                object obj = ResourceManager.GetObject("sprite_Human1_walk4", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
         internal static System.Drawing.Bitmap sprite_Skel1_attack0 {
